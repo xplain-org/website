1442c8039c65441e9ee39002a4727c6cd925b98e
root <root@PLEN-MP2E414N.UM6P.local>
2024-07-14 18:50:42 +0100

    feat(ui): add navbar and hero section

---
+++ src/app/page.tsx
@@ -0,0 +1,141 @@
+"use client";
+import Image from "next/image";
+import { useEffect, useState, useRef, lazy, Suspense } from "react";
+import { motion } from "framer-motion";
+
+const Navbar = lazy(() => import("@components/Navbar"));
+
+class MouseMovementObserver {
+  constructor() {
+    this.observers = [];
+    document.addEventListener("mousemove", this.notifyObservers.bind(this));
+  }
+
+  observers: ((event: MouseEvent) => void)[] = [];
+
+  subscribe(fn: (event: MouseEvent) => void) {
+    this.observers.push(fn);
+  }
+
+  unsubscribe(fn: (event: MouseEvent) => void) {
+    this.observers = this.observers.filter((observer) => observer !== fn);
+  }
+
+  notifyObservers(event: MouseEvent) {
+    this.observers.forEach((observer) => observer(event));
+  }
+}
+
+// Create a single instance of the observer
+const mouseMovementObserver = new MouseMovementObserver();
+
+interface GlowDotProps {
+  position: {
+    x: number;
+    y: number;
+  };
+}
+
+const GlowDot: React.FC<GlowDotProps> = ({ position }) => {
+  const dotRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const updatePosition = () => {
+      if (dotRef.current) {
+        dotRef.current.style.top = position.y + 20 + "px";
+        dotRef.current.style.left = position.x + 38 + "px";
+      }
+    };
+
+    const animationFrameId = requestAnimationFrame(updatePosition);
+
+    return () => {
+      cancelAnimationFrame(animationFrameId);
+    };
+  }, [position]);
+
+  return (
+    <motion.div
+      ref={dotRef}
+      style={{
+        position: "absolute",
+        // transition: 'top 0.1s ease, left 0.1s ease',
+      }}
+      transition={{ type: "spring", stiffness: 500, damping: 10 }}
+      id="glow-dot"
+    />
+  );
+};
+
+// export default GlowDot;
+
+export default function Home() {
+  const [dimensions, setDimensions] = useState({
+    width: 0,
+    height: 0,
+    top: 0,
+    left: 0,
+  });
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const updatePosition = (e: MouseEvent) => {
+      setPosition({ x: e.pageX, y: e.pageY });
+    };
+
+    mouseMovementObserver.subscribe(updatePosition);
+
+    return () => {
+      mouseMovementObserver.unsubscribe(updatePosition);
+    };
+  }, []);
+
+  return (
+    <>
+      <GlowDot position={position} />
+      <main className="flex min-h-screen flex-col items-center ">
+        <Suspense fallback={<div className="h-[112px] "></div>}>
+          <Navbar />
+        </Suspense>
+        <div className="flex flex-col justify-center items-center">
+          <motion.h1
+            className="text-[15vw] text-white font-bold shadow-lg tracking-wide shadow-lg font-outfit"
+            initial={{
+              opacity: 0,
+              scale: 0.7,
+            }}
+            animate={{
+              opacity: 1,
+              scale: 1,
+            }}
+            transition={{
+              duration: 1,
+              type: "linear",
+            }}
+          >
+            xplain
+          </motion.h1>
+          <motion.p
+            className="text-2xl text-white font-bold  font-outfit"
+            initial={{
+              y: "15vh",
+              opacity: 0,
+            }}
+            animate={{
+              y: 0,
+              opacity: 1,
+            }}
+            transition={{
+              delay: 0.5,
+              duration: 0.5,
+              type: "linear",
+            }}
+          >
+            Developement Studio
+          </motion.p>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,149 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+const AnimatedButton: React.FC = () => {
+  const [hovered, setHovered] = useState(false);
+
+  const variants = {
+    initial: {
+      scale: 1,
+    },
+    hover: {
+      scale: 8, // Scale up when hovered
+      transition: {
+        duration: 1, // Duration of the scale animation
+        ease: "easeInOut",
+      },
+    },
+    animate: {
+      top: ["0%", "0%", "100%", "100%", "0%"],
+      left: ["0%", "100%", "100%", "0%", "0%"],
+      transition: {
+        duration: 4,
+        ease: "linear",
+        // times: [0, 0.5, 0.5, 0.5, ],
+        repeat: Infinity,
+      },
+    },
+  };
+
+  return (
+    <motion.button
+      className="relative  w-[158px] h-[58px] rounded-full overflow-hidden flex items-center justify-center"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      initial={{ x: "20vw", opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{
+        delay: 0.5,
+        duration: 1,
+        type: "linear",
+        // stiffness: 120,
+      }}
+    >
+      <motion.div
+        className="bg-[#2C2C2C] w-full h-full "
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 0.5 }}
+        transition={{
+          duration: 1,
+          type: "linear",
+        }}
+      ></motion.div>
+      <motion.div
+        id="glow-button"
+        className={`rounded-full absolute  `}
+        variants={variants}
+        animate={hovered ? "hover" : "animate"}
+        // animate="animate"
+      />
+      <div className="bg-black w-[154px] h-[54px] rounded-full absolute text-white flex items-center justify-center">
+        Book a call
+      </div>
+    </motion.button>
+  );
+};
+
+const Navbar = () => {
+  return (
+    <motion.div
+      className="w-full flex justify-between max-w-[1360px]  items-center  p-[24px]"
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+        transition: {
+          delay: 1,
+          duration: 1,
+          type: "linear",
+        },
+      }}
+    >
+      {/* <h1 className="text-white">LOGO</h1> */}
+      <motion.img
+        src="/logo.svg"
+        alt="LOGO"
+        className="text-white"
+        initial={{ x: "-20vw", opacity: 0 }} // Start off the screen to the right
+        animate={{ x: 0, opacity: 1 }} // Animate to its original position
+        transition={{
+          delay: 0.5,
+          duration: 1,
+          type: "linear",
+          // stiffness: 120,
+        }}
+      />
+      <nav className="hidden 2xl:flex  rounded-full border-2 border-[#2C2C2C] p-2 ">
+        <div className="flex group hover:cursor-pointer flex-row items-center w-[103px] h-[44px] justify-center  gap-2 rounded-full hover:border-2 hover:border-[#2C2C2C]">
+          <div className="h-[6px] w-[6px] bg-[#999999] rounded-full group-hover:bg-[#A7F10C] "></div>
+          <a
+            href="#process"
+            className="text-[#999999] group-hover:text-white font-medium text-xs"
+          >
+            Process
+          </a>
+        </div>
+        <div className="flex group flex-row items-center  hover:cursor-pointer justify-center   w-[103px] h-[44px] gap-2 rounded-full hover:border-2 hover:border-[#2C2C2C]">
+          <div className="h-[6px] w-[6px] bg-[#999999] rounded-full  group-hover:bg-[#A7F10C]"></div>
+          <a
+            href="#process"
+            className="text-[#999999] group-hover:text-white font-medium text-xs"
+          >
+            Works
+          </a>
+        </div>
+        <div className="flex flex-row group items-center justify-center  hover:cursor-pointer w-[103px] h-[44px] gap-2 rounded-full hover:border-2 hover:border-[#2C2C2C]">
+          <div className="h-[6px] w-[6px] bg-[#999999] rounded-full group-hover:bg-[#A7F10C]"></div>
+          <a
+            href="#process"
+            className="text-[#999999] group-hover:text-white font-medium text-xs "
+          >
+            Services
+          </a>
+        </div>
+        <div className="flex flex-row group items-center justify-center   hover:cursor-pointer w-[103px] h-[44px] gap-2 rounded-full hover:border-2 hover:border-[#2C2C2C]">
+          <div className="h-[6px] w-[6px] bg-[#999999] rounded-full group-hover:bg-[#A7F10C]"></div>
+          <a
+            href="#process"
+            className="text-[#999999] group-hover:text-white font-medium text-xs"
+          >
+            Pricing
+          </a>
+        </div>
+        <div className="flex group flex-row items-center justify-center hover:cursor-pointer w-[103px] h-[44px] gap-2 rounded-full hover:border-2 hover:border-[#2C2C2C]">
+          <div className="h-[6px] w-[6px] bg-[#999999] rounded-full group-hover:bg-[#A7F10C]"></div>
+          <a
+            href="#process"
+            className="text-[#999999] group-hover:text-white font-medium text-xs"
+          >
+            Contact
+          </a>
+        </div>
+      </nav>
+      <AnimatedButton />
+    </motion.div>
+  );
+};
+
+export default Navbar;
